276681125e4909d49e720b508a9018328f6732bc
therealrinku <rinkunited2032@gmail.com>
2024-04-14 21:02:00 +0545

    chore: disable save button if there's no unsaved chanegs

--- src/renderer/components/Editor.tsx
+++ src/renderer/components/Editor.tsx
@@ -114,6 +114,7 @@ export default function Editor() {
 
             <div className="ml-auto flex flex-row items-center gap-2">
               <button
+                disabled={!haveUnsavedChanges}
                 onClick={() => handleSave(title, description)}
                 className="flex items-center text-xs bg-gray-100 hover:bg-gray-200 py-2 px-5 rounded"
               >
--- src/renderer/components/RenameModal.tsx
+++ src/renderer/components/RenameModal.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { FiX } from 'react-icons/fi';
+import { FiCheck, FiEdit2, FiEdit3, FiFilePlus, FiX } from 'react-icons/fi';
 
 interface Props {
   onClose: () => void;
@@ -15,10 +15,10 @@ export default function RenameModal({ onClose, initialText, onRename }: Props) {
       className="fixed top-0 left-0 w-screen h-screen flex flex-col items-center justify-center"
       style={{ backgroundColor: 'rgba(0,0,0,0.5)' }}
     >
-      <div className="relative flex flex-col gap-5 bg-white p-5 rounded">
+      <div className="relative flex flex-row items-center gap-2 bg-white w-72 p-5 py-12 rounded">
         <input
-          placeholder="Search..."
-          className="bg-gray-100 px-2 rounded w-full text-xs py-2 outline-none"
+          placeholder="Rename.."
+          className="bg-gray-100 px-2 rounded w-full text-xs py-2 outline-none w-[75%]"
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
@@ -26,12 +26,12 @@ export default function RenameModal({ onClose, initialText, onRename }: Props) {
         <button
           onClick={() => onRename(text)}
           disabled={!text.trim()}
-          className="flex items-center self-start text-xs bg-gray-100 hover:bg-gray-200 py-2 px-5 rounded disabled:opacity-70"
+          className="text-xs bg-gray-100 hover:bg-gray-200 py-2 px-5 rounded disabled:opacity-70"
         >
-          <p>Rename</p>
+          Save
         </button>
 
-        <button className="absolute top-1 right-2" onClick={onClose}>
+        <button className="absolute top-2 right-3" onClick={onClose}>
           <FiX />
         </button>
       </div>
