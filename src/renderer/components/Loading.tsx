18f873bd0364db5cbc6723e44835a631624f075a
therealrinku <rinkunited2032@gmail.com>
2024-07-03 11:27:20 +0545

    fix: light mode loading icon color

--- src/renderer/components/Loading.tsx
+++ src/renderer/components/Loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen w-screen text-white bg-white dark:bg-[#1e1e1e]">
+    <div className="flex flex-col items-center justify-center h-screen w-screen text-black dark:text-white bg-white dark:bg-[#1e1e1e]">
       <div className="lds-ripple">
         <div></div>
         <div></div>
