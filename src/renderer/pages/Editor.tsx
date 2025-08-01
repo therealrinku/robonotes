53f2f6628ae7a295979ad90aeb288983fe74749a
therealrinku <rinkunited2032@gmail.com>
2025-08-01 10:46:12 +0545

    feat: editor ui fixes and added tags count too

--- src/renderer/pages/Editor.tsx
+++ src/renderer/pages/Editor.tsx
@@ -15,6 +15,8 @@ export default function Editor() {
   const timeout0 = useRef<NodeJS.Timeout | null>(null);
 
   const wordCount = useMemo(() => content.split(/\s+/).filter((word) => word !== '').length, [content]);
+  const tagsCount = useMemo(() => content.split(" ").filter(word => word.includes("#")).length ,[content]);
+  
   const haveUnsavedChanges = note.content !== content;
   
   useEffect(()=>{
@@ -32,14 +34,14 @@ export default function Editor() {
   return (
     <div className="w-full max-h-[100vh] overflow-hidden bg-white dark:bg-[#1e1e1e]">
 
-      <div className="relative w-full text-sm mt-5">
+      <div className="relative w-full text-sm">
         <div className="flex flex-col h-[100vh] overflow-y-auto">
-          <button className="outline-none flex items-center text-xs" onClick={()=>navigate(-1)}>
-            <GoTriangleLeft size={20}/>
+          <button className="border-b outline-none px-3 py-2 flex items-center text-xs" onClick={()=>navigate(-1)}>
+            <GoTriangleLeft size={20}/> back
           </button>
 
           <textarea
-            className="w-full outline-none h-full px-6 bg-inherit"
+            className="w-full outline-none h-[90vh] px-6 pt-2 bg-inherit"
             placeholder="My important note..."
             value={content}
             autoCorrect="off"
@@ -48,12 +50,15 @@ export default function Editor() {
             onChange={(e) => setContent(e.target.value)}
           />
 
-          <div className="w-full py-1 flex items-center gap-4 text-xs justify-end pr-3 fixed bottom-0 bg-gray-200 dark:bg-[#1e1e1e]">
+          <div className="w-full py-1 flex items-center gap-4 text-xs justify-end pr-3 fixed bottom-0 bg-gray-100 dark:bg-[#1e1e1e]">
             <p>
               <span className="font-bold">{content.length}</span> characters
             </p>
             <p>
               <span className="font-bold">{wordCount}</span> words
+            </p>
+            <p>
+              <span className="font-bold">{tagsCount}</span> tags
             </p>
             <p className={`${haveUnsavedChanges ? "bg-red-500" : "bg-green-500"} h-2 w-2 rounded-full`}></p>
           </div>
