b7821b8f46ebede19c853fa9e7597b898ff3d480
therealrinku <rinkunited2032@gmail.com>
2025-12-21 11:39:37 +0545

    Move bttom bar to single toolbar up top

--- src/renderer/components/Toolbar.tsx
+++ src/renderer/components/Toolbar.tsx
@@ -3,14 +3,27 @@ import { GoStack, GoTag, GoTypography, GoTrash, GoPlus, GoX, GoSearch } from 're
 import useNotes from '../hooks/useNotes';
 
 interface Props{
+  selectedNote: any,
   onSelectNote?:(id: number) => void
 }
 
-export default function SearchModal({onSelectNote}:Props) {
+export default function Toolbar({selectedNote, onSelectNote}:Props) {
   const [searchQuery, setSearchQuery] = useState('');
   const [isFocused, setIsFocused] = useState(false);
 
   const { notes, handleCreateNewNote } = useNotes();
+  const content = selectedNote?.content || '';
+
+  const wordCount = useMemo(
+    () => content.split(/\s+/).filter((word) => word !== '').length,
+    [content],
+  );
+  const tagsCount = useMemo(
+    () =>
+      content.split(/\s+|\n+/).filter((word) => word.startsWith('#')).length,
+    [content],
+  );
+
 
   const filteredNotes = useMemo(() => {
     if (
@@ -37,12 +50,10 @@ export default function SearchModal({onSelectNote}:Props) {
     });
   }, [searchQuery, notes]);
 
-  const widthClasses = isFocused ? 'w-[50%]' : 'w-[25%]';
-
   return (
     <div className="flex flex-col items-center gap-3 w-full">
-      <div className=" w-full flex items-center gap-3 justify-between relative">
-        <div className={`flex items-center mx-auto ${widthClasses} bg-gray-200 dark:bg-[#1e1e1e] h-8`}>
+      <div className="flex items-center justify-between gap-2">
+        <div className="flex items-center bg-gray-200 dark:bg-[#1e1e1e] h-8">
           <GoSearch className="absolute ml-2 " color="gray" />
           <input
             title="Search with note content or by tag name, #tagname, note content"
@@ -63,28 +74,27 @@ export default function SearchModal({onSelectNote}:Props) {
             <button onClick={handleCreateNewNote} className="px-3 border-l h-full dark:border-gray-700 hover:bg-green-900">
               <GoPlus />
             </button>
+          </div>
+        </div>
+
+           <div className="flex items-center  text-xs justify-end bg-gray-100 dark:bg-[#1e1e1e] dark:text-white h-8">
+            <p className="flex items-center gap-2 px-3 border-r dark:border-gray-700 h-full" title={`${content.length} characters`}>
+              <span className="font-bold">{content.length}</span> <GoTypography/>
+            </p>
+            <p className="flex items-center gap-2 px-3 dark:border-gray-700 h-full"  title={`${wordCount} words`}>
+              <span className="font-bold">{wordCount}</span> <GoStack/>
+            </p>
+            <p className="flex items-center gap-2 px-3 border-l dark:border-gray-700 h-full" title={`${tagsCount} tags`}>
+              <span className="font-bold">{tagsCount}</span> <GoTag/>
+            </p>
             <button onClick={handleCreateNewNote} className="px-3 hover:bg-red-900 h-full dark:border-gray-700 border-l">
               <GoTrash size={11}/>
             </button>
           </div>
-        </div>
-
-
-          <div className="flex items-center  text-xs justify-end bg-gray-100 dark:bg-[#1e1e1e] dark:text-white mx-5 h-8">
-            <p className="flex items-center gap-2 px-3 border-r dark:border-gray-700 h-full" title="20 characters">
-              <span className="font-bold">20</span> <GoTypography/>
-            </p>
-            <p className="flex items-center gap-2 px-3 dark:border-gray-700 h-full"  title="200 words">
-              <span className="font-bold">200</span> <GoStack/>
-            </p>
-            <p className="flex items-center gap-2 px-3 border-l dark:border-gray-700 h-full" title="20 tags">
-              <span className="font-bold">200</span> <GoTag/>
-            </p>
-          </div>
       </div>
 
       {searchQuery.trim().length === 0 && isFocused && (
-        <div className={`absolute mt-8 mx-auto text-xs dark:text-white z-50 ${widthClasses}`}>
+        <div className='absolute mt-8 w-[100%] left-0 right-0 text-xs dark:text-white z-50'>
           <div className="bg-gray-200 dark:bg-[#1e1e1e] px-3 py-2 border-gray-200 dark:border-gray-700 border-t ">
             <h4 className="text-gray-500">Recently opened</h4>
             <p>Note1</p>
@@ -92,7 +102,7 @@ export default function SearchModal({onSelectNote}:Props) {
         </div>
       )}
 
-      <div className={`absolute mt-8 w-[85%] md:w-[50%] mx-auto text-xs dark:text-white z-50 ${widthClasses}`}>
+      <div className='absolute left-0 right-0 mt-8 mx-auto text-xs dark:text-white z-50'>
         {searchQuery.length > 0 && filteredNotes.length > 0 ? (
           <div className="bg-gray-200 dark:bg-[#1e1e1e] py-2 border-gray-200 dark:border-gray-700 border-t flex flex-col gap-3">
             <h4 className="text-gray-500 px-3">Results</h4>
--- src/renderer/pages/Editor.tsx
+++ src/renderer/pages/Editor.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo, useRef, useState } from 'react';
 import useNotes from '../hooks/useNotes';
 import { GoTrash, GoTriangleLeft } from 'react-icons/go';
-import SearchModal from '../components/SearchModal.tsx';
+import Toolbar from '../components/Toolbar';
 
 export default function Editor() {
   const { notes, handleUpdateNote, handleDeleteNote } = useNotes();
@@ -49,12 +49,11 @@ export default function Editor() {
 
   return (
     <div className="w-full max-h-[100vh] overflow-hidden bg-white dark:bg-[#303030] pt-3">
-      <SearchModal onSelectNote={(id) => handleSelectNote(id)} />
+      <Toolbar selectedNote={note} onSelectNote={(id) => handleSelectNote(id)} />
 
-      <div className="relative w-full text-sm">
         <div className="flex flex-col h-[100vh] overflow-y-auto border-t dark:border-gray-700 mt-3">
           <textarea
-            className="w-full outline-none h-full px-3 pt-1 bg-inherit dark:text-white border-b dark:border-gray-700 mb-[70px] pb-3"
+            className="w-full outline-none h-full px-3 pt-1 bg-inherit dark:text-white"
             placeholder="My important note..."
             value={content}
             autoCorrect="off"
@@ -62,22 +61,6 @@ export default function Editor() {
             autoFocus={true}
             onChange={(e) => setContent(e.target.value)}
           />
-
-          <div className="fixed bottom-0 w-full p-2 flex items-center gap-4 text-xs justify-end bg-gray-100 dark:bg-[#1e1e1e] dark:border-gray-700 dark:text-white border-l">
-            <p>
-              <span className="font-bold">{content.length}</span> characters
-            </p>
-            <p>
-              <span className="font-bold">{wordCount}</span> words
-            </p>
-            <p>
-              <span className="font-bold">{tagsCount}</span> tags
-            </p>
-            <p
-              className={`${haveUnsavedChanges ? 'bg-red-500' : 'bg-green-500'} h-2 w-2 rounded-full`}
-            ></p>
-          </div>
-        </div>
       </div>
     </div>
   );
